ea190b845ea5e90201707ae9bcf9485ca1a7e5da
bruno.neto <brunim.ipb@gmail.com>
2020-07-05 03:48:47 -0300

    define url da api

--- src/http-common.js
+++ src/http-common.js
@@ -3,8 +3,9 @@ import axios from 'axios';
 //Define a URL base da origem para consumo do servico
 // localmente, para testes, em baseURL usar 'http://localhost:3001/'
 // enquanto o react vai usar a 3000
+// define o app do heroku onde está buscando a api dos dados do MongoDB
 export default axios.create({
-  baseURL: 'http://localhost:3001/',
+  baseURL: 'https://majestic-yellowstone-34488.herokuapp.com/',
   headers: {
     'Content-type': 'application/json',
   },
